5526dd2607f7bab69defd2f532c6db71beb9c91c
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-20 08:35:58 +0300

    Домашка1 с циклами

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,19 @@ public class Main {
                 System.out.println(i + " год является високосным");
             }
         }
+        System.out.println();
+        {
+            // Задача № 6
+            for (int i = 7; i <= 98; i = i + 7) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        {
+            // Задача № 7
+            for (int i = 1; i <= 512; i = i * 2) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
